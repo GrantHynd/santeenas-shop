d8cbc7346784379f99bb123c7e742483250377f5
Grant Hynd <grant.hynd@bt.com>
2022-06-03 18:57:11 +0100

    Fetch products from API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import { Prisma, PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from "cors";
 
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/products", async (req, res) => {
   const { orderByPrice } = req.query;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,15 +8,36 @@ import {
   Grid,
   Typography,
 } from "@mui/material";
+import { useCallback, useEffect, useState } from "react";
+
+type Product = {
+  id: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  price: number;
+  stock: number;
+};
 
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const fetchProducts = useCallback(async () => {
+    const response = await fetch("http://localhost:8000/products/");
+    const data: Product[] = await response.json();
+    setProducts(data);
+  }, [setProducts]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
   return (
     <div className="container">
       <Head>
         <title>Nails by Santeena</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
         <Grid
           item
@@ -44,120 +65,33 @@ export default function Home() {
           direction="row"
           justifyContent=""
         >
-          {/* Product 1 */}
-          <Card sx={{ maxWidth: 320 }}>
-            <Image
-              layout="responsive"
-              width="320"
-              height="400"
-              src="https://dummyimage.com/630x810/ed407a/fff.jpg&text=product"
-              alt="product placeholder"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Product 1
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                tempor lorem.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Add to cart</Button>
-            </CardActions>
-          </Card>
-
-          {/* Product 2 */}
-          <Card sx={{ maxWidth: 320 }}>
-            <Image
-              layout="responsive"
-              width="320"
-              height="400"
-              src="https://dummyimage.com/630x810/ed407a/fff.jpg&text=product"
-              alt="product placeholder"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Product 2
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                tempor lorem.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Add to cart</Button>
-            </CardActions>
-          </Card>
-
-          {/* Product 3 */}
-          <Card sx={{ maxWidth: 320 }}>
-            <Image
-              layout="responsive"
-              width="320"
-              height="400"
-              src="https://dummyimage.com/630x810/ed407a/fff.jpg&text=product"
-              alt="product placeholder"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Product 3
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                tempor lorem.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Add to cart</Button>
-            </CardActions>
-          </Card>
-
-          {/* Product 4 */}
-          <Card sx={{ maxWidth: 320 }}>
-            <Image
-              layout="responsive"
-              width="320"
-              height="400"
-              src="https://dummyimage.com/630x810/ed407a/fff.jpg&text=product"
-              alt="product placeholder"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Product 4
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                tempor lorem.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Add to cart</Button>
-            </CardActions>
-          </Card>
-
-          {/* Product 5 */}
-          <Card sx={{ maxWidth: 320 }}>
-            <Image
-              layout="responsive"
-              width="320"
-              height="400"
-              src="https://dummyimage.com/630x810/ed407a/fff.jpg&text=product"
-              alt="product placeholder"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Product 5
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                tempor lorem.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Add to cart</Button>
-            </CardActions>
-          </Card>
+          {products.map((product) => {
+            return (
+              <Card key={product.id} sx={{ width: 320 }}>
+                <Image
+                  layout="responsive"
+                  width="320"
+                  height="400"
+                  src={product.imageUrl}
+                  alt={product.description}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {product.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {product.description}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    £{product.price}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Add to cart</Button>
+                </CardActions>
+              </Card>
+            );
+          })}
         </Grid>
       </main>
     </div>
