9c819a741b2a390089d91f82b38ea1db3cdb0d62
Grant Hynd <grant.hynd@gmail.com>
2023-02-19 11:49:36 +0000

    refactor: navigation with tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      'primary': '#ED4078',
+      'currentColor': 'currentColor',
+      'primary': {
+        DEFAULT: '#ED4078',
+        50: '#FDE8EF',
+        100: '#FBD5E1',
+        200: '#F8B0C7',
+        300: '#F48BAD',
+        400: '#F16592',
+        500: '#ED4078',
+        600: '#E01557',
+        700: '#AD1043',
+        800: '#790B2F',
+        900: '#46071B'
+      },
       'white': '#fff',
     },
   },
